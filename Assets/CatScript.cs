90594bf092c82f18916aaae521b85f5235b5bc0b
sashaggdev <arcanasasha@gmail.com>
2026-01-16 11:13:18 -0500

    Got sound working, probably in an overly complicated way.

--- Assets/CatScript.cs
+++ Assets/CatScript.cs
@@ -5,9 +5,9 @@ public class CatScript : MonoBehaviour
     public SpriteRenderer spriteRenderer;
     public Sprite openSprite;
     public Sprite closedSprite;
+    public AudioSource audioSource;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    //test
     void ChangeSprite(Sprite newSprite)
     {
         spriteRenderer.sprite = newSprite;
@@ -22,8 +22,8 @@ public class CatScript : MonoBehaviour
     // Update is called once per frame
     void OnMouseOver()
     {
-        Debug.Log("Mouse is over the cat!");
         ChangeSprite(openSprite);
+        AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
     }
 
     void OnMouseExit()
