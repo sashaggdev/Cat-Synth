19ccbee866e6cf1163d4cb1cb3a2ecab133b62f5
sashaggdev <arcanasasha@gmail.com>
2026-01-16 11:24:19 -0500

    Make the cat animate on hover. Could be better though.

--- Assets/CatScript.cs
+++ Assets/CatScript.cs
@@ -6,6 +6,8 @@ public class CatScript : MonoBehaviour
     public Sprite openSprite;
     public Sprite closedSprite;
     public AudioSource audioSource;
+    public float t = 0;
+    public AnimationCurve curve;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void ChangeSprite(Sprite newSprite)
@@ -19,11 +21,24 @@ public class CatScript : MonoBehaviour
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
+    private void Update()
+    {
+        t += Time.deltaTime;
+        if (t > 1)
+        {
+            t = 0;
+        }
+    }
+
     // Update is called once per frame
     void OnMouseOver()
     {
         ChangeSprite(openSprite);
         AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+
+        float y = curve.Evaluate(t);
+
+        transform.localScale = new Vector3(1 + y * 0.3f, 1 + y * 0.3f, 1);
     }
 
     void OnMouseExit()
